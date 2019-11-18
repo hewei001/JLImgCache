73e77f8daebe23d7e18038f5cafd53b2823d593e
人人咖 <zl@Levy.local>
2019-11-18 18:05:16 +0800

    “注释”

---
+++ JLImgCache/JLImgCache/JLImgView.h
@@ -0,0 +1,17 @@
+//
+//  JLImgView.h
+//  JLImgCache
+//
+//  Created by 人人咖 on 2019/11/18.
+//  Copyright © 2019 WZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JLImgView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JLImgCache/JLImgCache/JLImgView.m
@@ -0,0 +1,21 @@
+//
+//  JLImgView.m
+//  JLImgCache
+//
+//  Created by 人人咖 on 2019/11/18.
+//  Copyright © 2019 WZC. All rights reserved.
+//
+
+#import "JLImgView.h"
+
+@implementation JLImgView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
